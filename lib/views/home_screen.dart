679d4ff6a159dc41e49c6e7067fbb4f3c060cdca
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2023-09-12 06:25:08 +0100

    update

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -14,15 +14,30 @@ class HomeScreenView extends StatelessWidget {
       backgroundColor: const Color(0xffCEDEBD),
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 5),
+          padding: const EdgeInsets.symmetric(horizontal: 10),
           child: Column(
             children: <Widget>[
               const HomeHeaderWidget(),
               SizerHelper.verticalSpace(),
               const HomeProfileNameWidget(),
-              SizerHelper.verticalSpace(),
+              SizerHelper.verticalSpace(25),
               const HomeAboutWidget(),
               const Spacer(),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.black87,
+                  elevation: 0,
+                  minimumSize: const Size(350, 36),
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(2)),
+                  ),
+                  backgroundColor: const Color(0xffFAF1E4),
+                ),
+                child: const Text("Edit Details"),
+              ),
+              SizerHelper.verticalSpace(20),
             ],
           ),
         ),
--- lib/widgets/home/home_about_widget.dart
+++ lib/widgets/home/home_about_widget.dart
@@ -1,27 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:hng_stage_two/shared/sizer_helper.dart';
+
+import '../../shared/sizer_helper.dart';
 
 class HomeAboutWidget extends StatelessWidget {
   const HomeAboutWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-      width: double.infinity,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(5),
-        color: const Color(0xffFAF1E4),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          buildInfoWidget(
-            "About Me",
-            "I am a highly experienced Flutter developer with few years of professional experience in the field.  I am also well-versed in various state management techniques in Flutter such as GetX, BLoC, Provider, and Riverpod, and have experience working with external APIs and SDKs",
-          ),
-        ],
-      ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        buildInfoWidget(
+          "About",
+          "I am a highly experienced Flutter developer with few years of professional experience in the field.  I am also well-versed in various state management techniques in Flutter such as GetX, BLoC, Provider, and Riverpod, and have experience working with external APIs and SDKs",
+        ),
+      ],
     );
   }
 
@@ -37,7 +30,7 @@ class HomeAboutWidget extends StatelessWidget {
             color: Colors.black.withOpacity(0.8),
           ),
         ),
-        SizerHelper.verticalSpace(5),
+        SizerHelper.verticalSpace(),
         Text(
           value,
           style: TextStyle(
@@ -50,4 +43,20 @@ class HomeAboutWidget extends StatelessWidget {
       ],
     );
   }
+
+  // Widget buildInfoWidget(String title, String value) {
+  //   return Column(
+  //     crossAxisAlignment: CrossAxisAlignment.start,
+  //     children: <Widget>[
+  //       Text(
+  //         title,
+  //         style: const TextStyle(fontSize: 12, fontWeight: FontWeight.w300),
+  //       ),
+  //       Text(
+  //         value,
+  //         style: const TextStyle(fontSize: 14, fontWeight: FontWeight.w400),
+  //       ),
+  //     ],
+  //   );
+  // }
 }
